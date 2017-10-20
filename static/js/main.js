89a88c2555473861223ab299ea8824596e595d04
bmy <mingming.bai@yahoo.com>
2017-10-20 16:59:09 +0800

    add main.js

---
+++ static/js/main.js
@@ -0,0 +1,16 @@
+$(function() {
+    
+    $('.btn').click(function(){
+        var text = $('.text').val();
+        $.ajax({
+            url: '/api/taxonomy',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({text:text}),
+            success: function(res){
+                result = res.result;
+                $('.result').text(result);
+            }
+        });
+    })
+})
